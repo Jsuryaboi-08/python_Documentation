b4a53eed31031d1ace4aa18bc6e945b83a32289a
Jsuryaboi_08 <winjeyasurya@gmail.com>
2024-03-05 23:42:10 +0530

    forLoops

---
+++ for_loop.py
@@ -0,0 +1,18 @@
+#for loops
+import time
+for i in range(5):
+    print(i)
+
+#range(start, stop, step)
+
+for j in range(5, 10):
+    print(j)
+
+for k in range(0, 10, 2):
+    print(k)
+
+for seconds in range(10, 0, -1):
+    print(seconds)
+    time.sleep(1)
+print("Blastoff!")
+
