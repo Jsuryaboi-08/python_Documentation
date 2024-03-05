3ba76a92d6b4d4c0c95848a3bf914e1e51d4f724
Jsuryaboi_08 <winjeyasurya@gmail.com>
2024-03-05 23:23:50 +0530

    math functions

---
+++ Mathfunctions.py
@@ -0,0 +1,17 @@
+import math
+pi = -3.14
+
+A = 21;
+B = 22;
+
+print(round(pi))#rounds to the nearest whole number
+print(math.floor(pi))#rounds down
+print(math.ceil(pi))#rounds up
+print(abs(pi))#absolute value
+print(pow(pi, 2))#power
+print(math.sqrt(pi))#square root
+print(math.sin(pi))#sine
+print(math.cos(pi))#cosine
+print(math.tan(pi))#tangent
+print(math.max(A, B))#maximum
+print(math.min(A, B))#minimum
