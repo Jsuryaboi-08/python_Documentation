45a733c5bb303a7cffbb3cc345cf528a3f7be0c4
Jsuryaboi_08 <winjeyasurya@gmail.com>
2024-03-05 23:16:31 +0530

    python documentation

---
+++ Typecasting.py
@@ -0,0 +1,31 @@
+x = 1  #int
+y = 2.0#float
+z = "69"#string
+p = "420"#string
+#temporary changes
+print(x)
+print(int(y))
+print(int(z))
+print(int(z)*3)
+
+print(float(x))
+print(float(z))
+print(float(y))
+
+print(str(x))
+print(str(y))
+print(str(z))
+
+#permanent changes
+x = int(x)
+y = int(y)
+z = int(z)
+print(x)
+print(y/2)
+print(z)
+
+
+#use:
+print(x + int(p));
+
+
