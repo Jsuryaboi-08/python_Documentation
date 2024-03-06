eae94f0b4e33206cb3696c53e337397826fd0402
Jsuryaboi_08 <winjeyasurya@gmail.com>
2024-03-07 00:04:17 +0530

    lists useful functions

--- Lists.py
+++ Lists.py
@@ -12,3 +12,19 @@ for x in food:
 #negative indexing
 print (food[-1])
 
+food.append("pulav")
+print(food)
+food.remove("poori")
+print(food)
+food.pop()
+print(food)
+food.pop(2)#remove by index
+print(food)
+food.insert(2, "vada")
+print(food)
+food.sort()#sort in alphabetical order
+print(food)
+food.clear()#clear the list
+
+
+
