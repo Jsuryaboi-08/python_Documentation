79100dbd048ca8b8fa9afa18b477ef8edb34f141
Jsuryaboi_08 <winjeyasurya@gmail.com>
2024-03-07 00:02:55 +0530

    Lists in python

---
+++ Lists.py
@@ -0,0 +1,14 @@
+#multiple items store in a si9ngle variable
+
+#basically array list in java
+
+food = ["dosa", "idly", "vada", "poori", "chapathi", "biryani"]
+
+
+print (food[4])
+for x in food:
+    print(x)
+
+#negative indexing
+print (food[-1])
+
