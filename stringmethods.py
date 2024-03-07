902657c4743bbc6eb085faece186cf0d7487e011
Jsuryaboi_08 <winjeyasurya@gmail.com>
2024-03-07 22:20:47 +0530

    String format

--- stringmethods.py
+++ stringmethods.py
@@ -13,4 +13,35 @@ print(name.split(" "))
 print(name*3)
 
 
+#str.format
+#more control for displaying an output
 
+animal = "dog"
+action = "bite"
+print("Does your"+animal+action+"?")#Does your dog bite?
+print("Does your {} {}?".format(animal, action))#Does your dog bite?
+print("Does your {0} {1}?".format(animal, action))#Does your dog bite?
+print("Does your {animal} {action}?".format(animal = "cat", action = "bite"))#Does your cat bite?
+
+
+#.format() method takes the arguments stored in the format() method and places them in the string where the placeholders {} are
+
+name = "jeya surya"
+print("hello my name is {}".format(name))#hello my name is jeya surya
+print("hello my name is {:10}".format(name))#hello my name is jeya surya
+print("hello my name is {:<10}".format(name))#hello my name is jeya surya
+#< left align
+print("hello my name is {:>10}".format(name))#hello my name is jeya surya
+#> right align
+print("hello my name is {:^10}".format(name))#hello my name is jeya surya
+#^ center align
+
+#number formatting
+pi = 3.14159265359
+print("pi is equal to {}".format(pi))#pi is equal to 3.14159265359
+print("pi is equal to {:.2f}".format(pi))#pi is equal to 3.14
+print("pi is equal to {:.4f}".format(pi))#pi is equal to 3.1416
+print("pi is equal to {:b}".format(pi))#pi is equal to 11
+print("pi is equal to {:o}".format(pi))#pi is equal to 3 octal
+print("pi is equal to {:x}".format(pi))#pi is equal to 3.243f6a8885a3 hexadecimal
+print("pi is equal to {:e}".format(pi))#pi is equal to 3.141593e+00 scientific notation
