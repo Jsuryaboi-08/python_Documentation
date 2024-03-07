d2bb45f9d9b71402371139c223af155fe6ad6394
Jsuryaboi_08 <winjeyasurya@gmail.com>
2024-03-07 18:30:00 +0530

    functions

---
+++ function.py
@@ -0,0 +1,19 @@
+#functions a block of code that only runs when it is called
+
+def my_function(name,age):
+    print("Hello from a function")
+    print("My name is " + name)
+    print("My age is " + str(age))
+
+name = "surya"
+
+my_function(name,21)
+
+#return statement
+def add(a,b):
+    ADDED = a+b
+    return ADDED 
+
+print(add(2,3))
+
+
