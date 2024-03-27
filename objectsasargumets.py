95b9518827197cd697a99ab8eb20eb1134ba983e
Jsuryaboi_08 <winjeyasurya@gmail.com>
2024-03-27 21:59:54 +0530

    object as arguments in python

---
+++ objectsasargumets.py
@@ -0,0 +1,18 @@
+class Car:
+     color= None
+
+class Bike:
+    color = None
+def change_color(vehicle,color):  
+    vehicle.color = color
+
+car_1 = Car()
+car_2 = Car()
+bike_1 = Bike()
+change_color(car_1,"red")
+change_color(car_2,"blue")
+change_color(bike_1,"green")
+
+print(car_1.color)
+print(car_2.color)
+print(bike_1.color)
