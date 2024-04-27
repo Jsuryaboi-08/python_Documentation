d119e05f956264fba8331cfaa8e6cdfbce3bf7af
Jsuryaboi_08 <winjeyasurya@gmail.com>
2024-04-27 21:59:03 +0530

    assignming functions to variables

---
+++ functionstovariables.py
@@ -0,0 +1,16 @@
+def hello():
+    print("Hello, World!")
+
+hi = hello;
+hello()
+hi()
+
+# In this example, the function hello() is defined and then assigned to the variable hi.
+# The function is then called using both the function name and the variable name.
+# Both calls to the function will produce the same output, "Hello, World!".
+say = print
+
+say("Hello, World!")
+# In this example, the built-in print function is assigned to the variable say.
+# The say variable is then used to call the print function, which outputs "Hello, World!".
+# This demonstrates that functions can be assigned to variables and called using those variables.
